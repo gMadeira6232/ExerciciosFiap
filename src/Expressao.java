bcc6de2dc397682f93c4e15ae5be2ca3f66e48f5
Guilherme Madeira <guilhermemadeira@outlook.com>
2026-03-26 23:02:51 -0300

    Calculadora de expressao

---
+++ src/Expressao.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Expressao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x, y;
+
+        System.out.println("Digite o valor de X: ");
+        x = sc.nextDouble();
+
+        y = Math.pow(x-0.5, 1.0 / 6.0);
+        System.out.printf("Valor de Y: %.3f", y);
+    }
+}
