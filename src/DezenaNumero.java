51405cfb37c196b1ed61c1adf9b5a6ff81789d58
Guilherme Madeira <guilhermemadeira@outlook.com>
2026-03-27 01:37:25 -0300

    refatoracao descobridor de dezena de um numero

--- src/DezenaNumero.java
+++ src/DezenaNumero.java
@@ -7,7 +7,11 @@ public class DezenaNumero {
         System.out.println("Digite um numero: ");
         int valor = sc.nextInt();
 
-        System.out.println("A dezena do seu numero e: " + (valor / 10) % 10);
+        if (valor > 999 || valor < 99) {
+            System.out.println("Favor inserir um numero de 3 digitos");
+        } else {
+            System.out.println("A dezena do seu numero e: " + (valor / 10) % 10);
+        }
 
         sc.close();
     }
