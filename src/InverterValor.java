4087c206b159894f92c028cec30bbe536d4f8c50
Guilherme Madeira <guilhermemadeira@outlook.com>
2026-03-27 01:03:52 -0300

    refatoracao do Inversor de ordem de numeros

--- src/InverterValor.java
+++ src/InverterValor.java
@@ -3,18 +3,21 @@ import java.util.Scanner;
 public class InverterValor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int valor, unidade, dezena, centena, valorInvertido;
+        int valor, valorInvertido;
 
         System.out.println("Digite um numero: ");
         valor = sc.nextInt();
 
-        unidade = valor % 10;
-        dezena = (valor / 10) % 10;
-        centena = valor / 100;
-        valorInvertido = (unidade *100) + (dezena * 10) + centena;
+        valorInvertido = inversorDeValor(valor);
 
         System.out.println("Seu valor invertido e: " + valorInvertido);
 
         sc.close();
+    }
+    public static int inversorDeValor (int valor) {
+        int u = valor % 10;
+        int d = (valor / 10) % 10;
+        int c = valor / 100;
+        return (u * 100) + (d * 10) + c;
     }
 }
